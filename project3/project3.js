6df92faec8682d77d84c1c5fb7f01323b0df0b1e
Michael Hinton <michael.g.hinton@gmail.com>
2016-08-09 11:54:16 -0600

    Project 3: Created initial layout
    Got buttons hooked into javascript.
    Got the css to look decent.

--- project3/project3.js
+++ project3/project3.js
@@ -1,1 +1,26 @@
 console.log("Hello, World!");
+
+
+
+// TODO: Use loops to create a site that will randomly generate a number sequence
+// where one of the numbers is blanked out. Allow the user to put the missing number in.
+
+
+// document.querySelector('html').onclick = function() {
+//     alert('Ouch! Stop poking me!');
+// }
+
+
+function check_answer() {
+    console.log("Checking answer...");
+}
+
+
+function new_problem() {
+    console.log("Creating new problem...");
+}
+
+
+
+// TODO: Check to see if the number is correct and keep track of the number of correct and incorrect
+
