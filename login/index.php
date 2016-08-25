5e458907aa07ab3bdd4fe1088c1c9bcf85e4b16f
Michael Hinton <michael.g.hinton@gmail.com>
2016-08-25 11:49:42 -0600

    Project7: Started login project

---
+++ login/index.php
@@ -0,0 +1,13 @@
+ <?php
+    // Project 7 - Login
+
+    // Symlink the parent public "login" folder into MAMP/htdocs with the following command:
+    // ln -s /Users/mghinton/Documents/msedev_training/login /Applications/MAMP/htdocs/login
+    // Then input this link into the browser to execute this file:
+    // http://localhost:8888/login/
+
+    echo "Hello, World!";
+
+
+
+?>
