e0db83eeffab1c49b188f7f65ff17e2070d4724b
Michael Hinton <michael.g.hinton@gmail.com>
2016-08-29 14:48:47 -0600

    Brickus: Created nav links and checked user login for brickus.php

--- brickus/brickus.php
+++ brickus/brickus.php
@@ -1,7 +1,16 @@
 <?php
+    session_name("brickus_session_id");
+    session_start();
 
-    // TODO: Check to make sure the user is registered
+    // TODO: Check to make sure the user is logged in
+    if(!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] == false) {
+        echo "You are not logged in! Redirecting to login page...";
+        readfile("views/login.html");
+        exit();
+    }
 
+
+    // TODO: Check to make sure the user is part of the current game
     readfile("views/brickus.html");
 
 
